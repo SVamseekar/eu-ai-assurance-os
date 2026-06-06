ea67973707f1f4d6717e1a4f62523a8fb144ec72
Soura <martisoura@gmail.com>
2026-06-07 00:58:15 +0530

    fix(workflow): remove unused import in ApprovalWorkflowServiceTest

--- services/api/src/test/java/os/assurance/eu/api/workflow/ApprovalWorkflowServiceTest.java
+++ services/api/src/test/java/os/assurance/eu/api/workflow/ApprovalWorkflowServiceTest.java
@@ -3,7 +3,6 @@ package os.assurance.eu.api.workflow;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
